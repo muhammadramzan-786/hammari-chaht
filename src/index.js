c9d166c44893bce2b330536e85e871b9e2a29f27
Muhammad Ramzan <mr4323992@gmail.com>
2024-11-06 22:32:36 +0500

    some changings

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+// The App component is the main component in your app
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
